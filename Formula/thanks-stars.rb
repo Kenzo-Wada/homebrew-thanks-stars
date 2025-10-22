c4ae28da6b91788bdb6c2b49a1c787db955fa6f5
Kenzo-Wada <Kenzo-Wada@users.noreply.github.com>
2025-10-22 17:39:07 +0000

    thanks-stars 0.0.0

---
+++ Formula/thanks-stars.rb
@@ -0,0 +1,60 @@
+class ThanksStars < Formula
+  desc "Star the GitHub repositories backing your project's dependencies from the command line."
+  homepage "https://github.com/Kenzo-Wada/thanks-stars"
+  version "0.0.0"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/Kenzo-Wada/thanks-stars/releases/download/v0.0.0/thanks-stars-aarch64-apple-darwin.tar.gz"
+      sha256 "109a142ad4950ed56745eeaff3162537f09662b511a8a5c57078a961c3e04c2b"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/Kenzo-Wada/thanks-stars/releases/download/v0.0.0/thanks-stars-x86_64-apple-darwin.tar.gz"
+      sha256 "cdc2ccfb571a1c104240f386d15a4968b1d46d4cb01b35c42b310e302553f8ec"
+    end
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/Kenzo-Wada/thanks-stars/releases/download/v0.0.0/thanks-stars-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "1e24bb69dfdf5d2b3aa945adc02ed0b16ec2e299c61c0057624cf011ecdaafb9"
+  end
+  license "MIT"
+
+  BINARY_ALIASES = {
+    "aarch64-apple-darwin":              {},
+    "x86_64-apple-darwin":               {},
+    "x86_64-pc-windows-gnu":             {},
+    "x86_64-unknown-linux-gnu":          {},
+    "x86_64-unknown-linux-musl-dynamic": {},
+    "x86_64-unknown-linux-musl-static":  {},
+  }.freeze
+
+  def target_triple
+    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
+    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
+
+    "#{cpu}-#{os}"
+  end
+
+  def install_binary_aliases!
+    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
+      dests.each do |dest|
+        bin.install_symlink bin/source.to_s => dest
+      end
+    end
+  end
+
+  def install
+    bin.install "thanks-stars" if OS.mac? && Hardware::CPU.arm?
+    bin.install "thanks-stars" if OS.mac? && Hardware::CPU.intel?
+    bin.install "thanks-stars" if OS.linux? && Hardware::CPU.intel?
+
+    install_binary_aliases!
+
+    # Homebrew will automatically install these, so we don't need to do that
+    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
+    leftover_contents = Dir["*"] - doc_files
+
+    # Install any leftover files in pkgshare; these are probably config or
+    # sample files.
+    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
+  end
+end
